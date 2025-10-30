0934c12e13c4242c6c2add2e3358acc8f3b56ce6
Sundar Popuri <spop8394@uni.sydney.edu.au>
2025-10-30 18:02:05 +1100

    initial commits

--- data_split.py
+++ data_split.py
@@ -1,5 +1,6 @@
 
 from sklearn.model_selection import train_test_split
+import pandads as pd
 
 def split_data(dataset, y, test_size, val_size, random_state):
 
@@ -19,5 +20,20 @@ def split_data(dataset, y, test_size, val_size, random_state):
     )
 
     return X_train, X_val, X_test, y_train, y_val, y_test
+
+
+
+dataset1 = pd.read_csv('datasets/cleaned_housedata_sydney_enriched.csv') #<- this has the most predictors AND rows
+dataset2 = pd.read_csv('datasets/nsw_propertydata_sydney_cleaned.csv')
+dataset3 = pd.read_csv('datasets/suburb_yearly_bybednum_sydney_enriched.csv')
+
+# print(f"Dataset1 shape: {dataset1.shape}")
+# print(f"Dataset2 shape: {dataset2.shape}")
+# print(f"Dataset3 shape: {dataset3.shape}")  
+
+X_train, X_val, X_test, y_train, y_val, y_test = split_data(dataset1, 'price', test_size=0.2, val_size=0.25, random_state=42)  # 0.25 x 0.8 = 0.2    
+
+print(X_train.head())
+
 
 
---
+++ selection_and_split.py
@@ -0,0 +1,1 @@
+# this page will help us create a final data set for train/val/test after initial cleaning and feature engineering
